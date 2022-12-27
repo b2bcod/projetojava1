f743ea7e58115ec197b5bfeaac1ca3ed043b5c65
bruno <bruno@bruno-pc>
2022-12-26 21:08:03 -0400

    nova atualização do projeto

--- primeiro-programa/src/executavel/PrimeiraClasse.java
+++ primeiro-programa/src/executavel/PrimeiraClasse.java
@@ -2,7 +2,7 @@ package executavel;
 
 public class PrimeiraClasse {
 
-	/*Main é um método auto executável em Java!!!*/
+	/*Main é um método auto executável em Java!*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em Java");
 	}
